8fab8cd86f12846a49cb5f88ffa70ddc8662f194
Felipe Santos <fsantosmg@gmail.com>
2022-07-24 11:35:22 -0300

    tela de login

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,175 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/components/custom_text_field.dart';
+import 'package:greengrocer/src/config/curtom_colors.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //Nome do app
+                    Text.rich(
+                      TextSpan(
+                        style: const TextStyle(
+                          fontSize: 40,
+                        ),
+                        children: [
+                          const TextSpan(
+                            text: 'Feirinha',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Online',
+                            style: TextStyle(
+                              color: CustomColors.customContrastColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    //categorias
+                    SizedBox(
+                      height: 30,
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 25),
+                        child: AnimatedTextKit(
+                          pause: Duration.zero,
+                          repeatForever: true,
+                          animatedTexts: [
+                            FadeAnimatedText('Frutas'),
+                            FadeAnimatedText('Verduras'),
+                            FadeAnimatedText('Legumes'),
+                            FadeAnimatedText('Carnes'),
+                            FadeAnimatedText('Cereais'),
+                            FadeAnimatedText('Laticíneos'),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              //formulario
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 40,
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(45),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    //email
+                    const CustomTextField(
+                      icon: Icons.email,
+                      label: 'E-Mail',
+                    ),
+                    //senha
+                    const CustomTextField(
+                      icon: Icons.lock,
+                      label: 'Senha',
+                      isSecret: true,
+                    ),
+                    //botao entrar
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18))),
+                        onPressed: () {},
+                        child: const Text(
+                          "Entrar",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    //esqueceu a cenha
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Esqueceu a senha?',
+                          style: TextStyle(
+                            color: CustomColors.customContrastColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                    //divisor
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Text('Ou'),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    //Botão novo usuário
+                    SizedBox(
+                      height: 50,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          side: const BorderSide(
+                            width: 2,
+                            color: Colors.green,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text('Criar conta',
+                            style: TextStyle(
+                              fontSize: 18,
+                            )),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
